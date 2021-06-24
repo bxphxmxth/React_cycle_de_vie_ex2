7eb6fa1a30a9ffc5d0e3cc11ce4d6e0895117014
bxphxmxth <fushs@hotmail.fr>
2021-06-24 14:24:56 +0200

    f

--- src/App.js
+++ src/App.js
@@ -4,15 +4,25 @@ import React from 'react';
 
 class App extends React.Component {
 
+ componentDidMount(){
+          setInterval(() =>{
+               this.setState({
+
+                    dateCourante : new Date()
+               })
+          }, 1000)
+     }
+
+
   state = {
     dateCourante : new Date()
   }
 
   render(){
 
-    let jour = this.state.dateCourante.getDate();
-    let cMonth =this.state.dateCourante.getMonth() + 1;
-    let cYear =this.state.dateCourante.getFullYear();
+    // let jour = this.state.dateCourante.getDate();
+    // let cMonth =this.state.dateCourante.getMonth() + 1;
+    // let cYear =this.state.dateCourante.getFullYear();
     // {this.state.dateCourante.getDate()}/{this.state.dateCourante.getMonth()+1}/{this.state.dateCourante.getFullYear()}
     let time = this.state.dateCourante.getHours() + ":" + this.state.dateCourante.getMinutes() + ":" + this.state.dateCourante.getSeconds();
 
