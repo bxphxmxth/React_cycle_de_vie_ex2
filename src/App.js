d752b84b6b22eed6c86ed745163edc5ca2b0ddf6
bxphxmxth <fushs@hotmail.fr>
2021-06-24 14:13:56 +0200

    date

--- src/App.js
+++ src/App.js
@@ -9,10 +9,15 @@ class App extends React.Component {
   }
 
   render(){
+
+    let jour = this.state.dateCourante.getDate();
+    let cMonth =this.state.dateCourante.getMonth() + 1;
+    let cYear =this.state.dateCourante.getFullYear();
     
     return (
       <div className="App">
-        
+        {/* <p>Il est {this.state.dateCourante}</p> */}
+        <p>{jour}/{cMonth}/{cYear}</p>
   
       </div>
     );
